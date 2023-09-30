495b72880f1bf0d3b6ce0906a444827ae1e90f94
yasir-alobaidi <yasser.alobaydy@gmail.com>
2023-10-01 01:48:59 +0300

    activate HMR

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: true,
+    strictPort: true,
     port: 3000,
   },
   plugins: [react()],
