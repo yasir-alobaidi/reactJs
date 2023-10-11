0408e30a33b418aea62cf052435022d699a4b337
yasir-alobaidi <yasser.alobaydy@gmail.com>
2023-10-11 23:07:33 +0300

    edit operation

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,14 +3,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faToggleOff } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = ({task, toggleComplete, deleteTodo}) => {
+export const Todo = ({task, toggleComplete, editTodo, deleteTodo}) => {
   return (
     <>
       <div className='Todo'>
         <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed': ""}`}>
           {task.task}</p>
         <div>
-          <FontAwesomeIcon icon={faPenToSquare} />
+          <FontAwesomeIcon icon={faPenToSquare} 
+          onClick={() => editTodo(task.id)} />
           <FontAwesomeIcon icon={faTrash} 
             onClick={() => deleteTodo(task.id)} />
         </div>
